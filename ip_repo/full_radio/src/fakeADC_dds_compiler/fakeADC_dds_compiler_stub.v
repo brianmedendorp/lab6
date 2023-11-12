6d3128b03db1abc30299549e84d0cc7f53243436
brianmedendorp <bmedend1@jhu.edu>
2023-11-11 18:00:03 -0700

    files added

---
+++ ip_repo/full_radio/src/fakeADC_dds_compiler/fakeADC_dds_compiler_stub.v
@@ -0,0 +1,26 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+// Date        : Sat Nov 11 17:35:05 2023
+// Host        : TABLET-1FVECONB running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/brian/Documents/lab6/radio_periph_lab/ip_repo/full_radio/src/fakeADC_dds_compiler/fakeADC_dds_compiler_stub.v
+// Design      : fakeADC_dds_compiler
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "dds_compiler_v6_0_22,Vivado 2022.1" *)
+module fakeADC_dds_compiler(aclk, aresetn, s_axis_phase_tvalid, 
+  s_axis_phase_tdata, m_axis_data_tvalid, m_axis_data_tdata)
+/* synthesis syn_black_box black_box_pad_pin="aclk,aresetn,s_axis_phase_tvalid,s_axis_phase_tdata[31:0],m_axis_data_tvalid,m_axis_data_tdata[15:0]" */;
+  input aclk;
+  input aresetn;
+  input s_axis_phase_tvalid;
+  input [31:0]s_axis_phase_tdata;
+  output m_axis_data_tvalid;
+  output [15:0]m_axis_data_tdata;
+endmodule
---
+++ ip_repo/full_radio/src/tuner_dds_compiler/tuner_dds_compiler_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+-- Date        : Sat Nov 11 17:36:05 2023
+-- Host        : TABLET-1FVECONB running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/brian/Documents/lab6/radio_periph_lab/ip_repo/full_radio/src/tuner_dds_compiler/tuner_dds_compiler_stub.vhdl
+-- Design      : tuner_dds_compiler
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity tuner_dds_compiler is
+  Port ( 
+    aclk : in STD_LOGIC;
+    aresetn : in STD_LOGIC;
+    s_axis_phase_tvalid : in STD_LOGIC;
+    s_axis_phase_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    m_axis_data_tvalid : out STD_LOGIC;
+    m_axis_data_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end tuner_dds_compiler;
+
+architecture stub of tuner_dds_compiler is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,aresetn,s_axis_phase_tvalid,s_axis_phase_tdata[31:0],m_axis_data_tvalid,m_axis_data_tdata[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "dds_compiler_v6_0_22,Vivado 2022.1";
+begin
+end;
